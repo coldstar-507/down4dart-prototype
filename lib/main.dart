42b2f86115ac31443a326a983b173d711edd5454
coldstar-507 <scott.1212@gmx.com>
2022-04-13 17:51:36 -0400

    It's good

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'src/kernel.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
-// Test
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   var dir = await getApplicationDocumentsDirectory();
