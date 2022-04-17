e0f35bf1cc64a35439d4e1da02f56b7b366aca04
coldstar-507 <scott.1212@gmx.com>
2022-04-17 16:29:51 -0400

    big and important palette builder page prototype

--- lib/src/render_pages.dart
+++ lib/src/render_pages.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_testproject/src/data_objects.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'render_objects.dart';
-import 'package:file_picker/file_picker.dart';
 import 'dart:convert';
 
 class PalettePage extends StatelessWidget {
@@ -18,9 +17,7 @@ class PalettePage extends StatelessWidget {
         body: SafeArea(
             child: Container(
                 color: PinkTheme.backGroundColor,
-                child: Column(
-                  children: [paletteList, console],
-                ))));
+                child: Column(children: [paletteList, console]))));
   }
 }
 
@@ -32,11 +29,11 @@ class MessagePage extends StatelessWidget {
       : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Down4Container(
-        padding: 16.0,
-        backgroundColor: PinkTheme.backGroundColor,
+    return Container(
+        padding: const EdgeInsets.all(16.0),
+        color: PinkTheme.backGroundColor,
         child: Column(
-          children: [messageList, const Spacer(), console],
+          children: [messageList, console],
         ));
   }
 }
@@ -55,11 +52,13 @@ class AddFriendPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Down4Container(
-      backgroundColor: PinkTheme.backGroundColor,
+            child: Container(
+      color: PinkTheme.backGroundColor,
       child: Stack(
         children: [
-          QrImage(data: myID, foregroundColor: PinkTheme.qrColor),
+          Container(
+              padding: const EdgeInsets.all(16.0),
+              child: QrImage(data: myID, foregroundColor: PinkTheme.qrColor)),
           Column(children: [paletteList, console]),
         ],
       ),
@@ -72,9 +71,9 @@ class LoadingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Down4Container(
-      backgroundColor: PinkTheme.backGroundColor,
-      child: Center(child: Text("Loading...")),
+    return Container(
+      color: PinkTheme.backGroundColor,
+      child: const Center(child: Text("Loading...")),
     );
   }
 }
@@ -97,87 +96,65 @@ class _NodePageState extends State<NodePage> {
   }
 }
 
-class UserCreationPage extends StatefulWidget {
-  final void Function(Map<String, String>) callBack;
-  const UserCreationPage({Key? key, required this.callBack}) : super(key: key);
+class PaletteMakerPage extends StatefulWidget {
+  void Function(Map<String, Map<String, String>>) kernelInfoCallBack;
+  bool makingUser;
+  String? userID;
+  PaletteMakerPage(
+      {required this.kernelInfoCallBack,
+      this.makingUser = false,
+      this.userID,
+      Key? key})
+      : super(key: key);
+
   @override
-  State<UserCreationPage> createState() => _UserCreationPageState();
+  State<PaletteMakerPage> createState() => _PaletteMakerPageState();
 }
 
-class _UserCreationPageState extends State<UserCreationPage> {
-  Map<String, String> info = {};
+class _PaletteMakerPageState extends State<PaletteMakerPage> {
+  Map<String, Map<String, String>> infos = {};
+  late String at;
+  late String currentConsole;
+  late Map<String, Console> consoles;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.makingUser) {
+      currentConsole = "user";
+      at = "user";
+      infos["user"] = {"id": ""};
+    }
+    consoles = {
+      "user": Console(
+        textInputType: TextInputType.phone,
+        inputCallBack: (text) => infos["user"]!["phone"] = text,
+        placeHolder: "and your phone number.",
+        bottomButtons: [
+          ConsoleButton(name: "Camera", onPress: () => print("Todo")),
+          ConsoleButton(
+              name: "Proceed", onPress: () => widget.kernelInfoCallBack(infos))
+        ],
+      ),
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Down4Container(
-        padding: 16.0,
-        backgroundColor: PinkTheme.backGroundColor,
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const Spacer(),
-          Down4Container(
-              height: Palette.height,
-              child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  textDirection: TextDirection.ltr,
-                  children: [
-                    GestureDetector(
-                        onTap: () async {
-                          FilePickerResult? r = await FilePicker.platform
-                              .pickFiles(
-                                  type: FileType.custom,
-                                  allowedExtensions: ['jpg', 'png'],
-                                  withData: true);
-                          if (r != null) {
-                            setState(() {
-                              info['image'] =
-                                  base64Encode(r.files.single.bytes!);
-                            });
-                          }
-                        },
-                        child: Down4Container(
-                            backgroundColor: PinkTheme.headerColor,
-                            width: Palette.height,
-                            padding: 1.5,
-                            child: Center(
-                                child: info['image'] == null
-                                    ? Image.asset(
-                                        'lib/src/assets/picture_place_holder.png',
-                                        fit: BoxFit.cover,
-                                      )
-                                    : Image.memory(
-                                        base64Decode(info['image']!),
-                                        gaplessPlayback: true,
-                                        fit: BoxFit.cover,
-                                      )))),
-                    Expanded(
-                        child: Down4Container(
-                            paddingLeft: 10.0,
-                            paddingTop: 10.0,
-                            backgroundColor: PinkTheme.headerColor,
-                            child: TextField(
-                                textAlignVertical: TextAlignVertical.top,
-                                decoration: const InputDecoration(
-                                    hintText: "Pick a name and an image!",
-                                    border: InputBorder.none,
-                                    contentPadding: EdgeInsets.only(
-                                        bottom: (Palette.height) / 2)),
-                                textDirection: TextDirection.ltr,
-                                onChanged: ((value) =>
-                                    setState(() => info['name'] = value))))),
-                  ])),
-          const Spacer(),
-          Console(bottomButtons: [
-            ConsoleButton(
-                name: "Ok !",
-                onPress: () {
-                  if (info['image'] != null && info['name'] != null) {
-                    print("Lets go");
-                    widget.callBack(info);
-                  } else {
-                    print("Need name and image!");
-                  }
-                })
-          ])
-        ]));
+    return Container(
+      color: PinkTheme.backGroundColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          PaletteMakerList(
+              palettes: infos[at]!
+                  .map((key, value) => MapEntry(key,
+                      PaletteMaker(infoCallBack: (info) => infos[at] = info)))
+                  .values
+                  .toList()),
+          consoles[currentConsole]!
+        ],
+      ),
+    );
   }
 }
