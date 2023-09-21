13c668a454d4da47c189d3e95cf561ace16f17da
scott <scott.1212@gmx.com>
2023-09-20 21:59:42 -0400

    refractoring scan

--- lib/src/pages/_page_utils.dart
+++ lib/src/pages/_page_utils.dart
@@ -7,10 +7,12 @@ import 'package:down4/src/render_objects/palette.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
 
 import 'package:mime/mime.dart';
+
 // import 'package:mobile_scanner/mobile_scanner.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+// import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import '../_dart_utils.dart';
 
@@ -949,6 +951,74 @@ mixin Camera2 on Pager2 {
         },
       );
 
+  final _bs = BarcodeScanner(formats: [BarcodeFormat.qrCode]);
+
+  void _imageProcess(CameraImage image) async {
+    final im = _inputImageFromCameraImage(image);
+    if (im == null) return;
+    final codes = await _bs.processImage(im);
+    if (codes.isEmpty) return;
+  }
+
+  final _orientations = {
+    DeviceOrientation.portraitUp: 0,
+    DeviceOrientation.landscapeLeft: 90,
+    DeviceOrientation.portraitDown: 180,
+    DeviceOrientation.landscapeRight: 270,
+  };
+
+  InputImage? _inputImageFromCameraImage(CameraImage image) {
+    // get image rotation
+    // it is used in android to convert the InputImage from Dart to Java
+    // `rotation` is not used in iOS to convert the InputImage from Dart to Obj-C
+    // in both platforms `rotation` and `camera.lensDirection` can be used to compensate `x` and `y` coordinates on a canvas
+    final camera = cam;
+    final sensorOrientation = camera.sensorOrientation;
+    InputImageRotation? rotation;
+    if (Platform.isIOS) {
+      rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
+    } else if (Platform.isAndroid) {
+      var rotationCompensation =
+          _orientations[cameraController!.value.deviceOrientation];
+      if (rotationCompensation == null) return null;
+      if (camera.lensDirection == CameraLensDirection.front) {
+        // front-facing
+        rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
+      } else {
+        // back-facing
+        rotationCompensation =
+            (sensorOrientation - rotationCompensation + 360) % 360;
+      }
+      rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
+    }
+    if (rotation == null) return null;
+
+    // get image format
+    final format = InputImageFormatValue.fromRawValue(image.format.raw);
+    // validate format depending on platform
+    // only supported formats:
+    // * nv21 for Android
+    // * bgra8888 for iOS
+    if (format == null ||
+        (Platform.isAndroid && format != InputImageFormat.nv21) ||
+        (Platform.isIOS && format != InputImageFormat.bgra8888)) return null;
+
+    // since format is constraint to nv21 or bgra8888, both only have one plane
+    if (image.planes.length != 1) return null;
+    final plane = image.planes.first;
+
+    // compose InputImage using bytes
+    return InputImage.fromBytes(
+      bytes: plane.bytes,
+      metadata: InputImageMetadata(
+        size: Size(image.width.toDouble(), image.height.toDouble()),
+        rotation: rotation, // used only in Android
+        format: format, // used only in iOS
+        bytesPerRow: plane.bytesPerRow, // used only in iOS
+      ),
+    );
+  }
+
   String get backFromCameraConsoleName => "base";
   ConsoleButton get cameraCloseButton => ConsoleButton(
         name: "CLOSE",
@@ -1031,23 +1101,16 @@ mixin Camera2 on Pager2 {
       return Container(
           clipBehavior: Clip.hardEdge,
           decoration: const BoxDecoration(
-            borderRadius: BorderRadius.vertical(
-                top: Radius.circular(Pager2._widgetRadius)),
-            // border: Border.all(
-            //     color: g.theme.consoleBorderColor, width: Console.borderWidth),
-          ),
+              borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(Pager2._widgetRadius))),
           child: tempInput!.display(
               size: _squaredCamSize, forceSquare: true, autoPlay: true));
     } else if (cameraController != null) {
       return Container(
         clipBehavior: Clip.hardEdge,
         decoration: const BoxDecoration(
-          borderRadius:
-              BorderRadius.vertical(top: Radius.circular(Pager2._widgetRadius)),
-          // color: g.theme.consoleBorderColor,
-          // border: Border.all(
-          //     color: g.theme.consoleBorderColor, width: Console.borderWidth),
-        ),
+            borderRadius: BorderRadius.vertical(
+                top: Radius.circular(Pager2._widgetRadius))),
         child: SizedBox.square(
             dimension: Console.trueWidth,
             child: Transform.scale(
@@ -1146,7 +1209,6 @@ mixin Add2 {
 }
 
 mixin Scanner2 on Pager2 {
-
   void onScan(Barcode bc);
   Widget? _scanner;
 
@@ -1184,7 +1246,7 @@ mixin Scanner2 on Pager2 {
           // _scanner = null;
         } else {
           // Future.delayed(Andrew.pageSwitchAnimationDuration, loadScanner);
-          Future.delayed(const Duration(milliseconds: 300), loadScanner);          
+          Future.delayed(const Duration(milliseconds: 300), loadScanner);
         }
         setTheState();
       });
